120b8d88021235917432e75ae959317972d8b552
_efrainmejiasc <85888811+EfrainAS@users.noreply.github.com>
2022-03-13 14:48:24 -0400

    inicio

---
+++ AppPhone/AppPhone/AppPhone/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AppPhone.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AppPhone.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ AppPhone/AppPhone/AppPhone/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AppPhone.Models;
+using AppPhone.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppPhone.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
